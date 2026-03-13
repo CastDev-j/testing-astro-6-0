d0baa03c0e6536500422b57f334bfed55c270218
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-13 16:38:19 -0600

    Enhance project structure: add Footer and Social components, refactor About and Blog pages, and update Post layout with new interface for posts

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,99 @@ export const routes = [
   { name: "About", href: "/about" },
   { name: "Blog", href: "/blog" },
 ];
+
+export const socials = [
+  {
+    name: "Email",
+    text: "andres2004_cj@outlook.com",
+    href: "mailto:andres2004_cj@outlook.com",
+  },
+  {
+    name: "Github",
+    text: "CastDev-j",
+    href: "https://github.com/CastDev-j",
+  },
+];
+
+export const Certificates = [
+  {
+    title: "three.js journey",
+    url: "https://threejs-journey.com/certificate/view/48269",
+    logo: "https://threejs-journey.com/assets/favicons/favicon.ico",
+  },
+  {
+    title: "Next.js: El framework de React para producción",
+    url: "https://cursos.devtalles.com/certificates/bnf8ht3men",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "React PRO: Lleva tus bases al siguiente nivel",
+    url: "https://cursos.devtalles.com/certificates/es1w9lytur",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "React: De cero a experto ( Hooks y MERN )",
+    url: "https://cursos.devtalles.com/certificates/beksnnc5mi",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Shadcn/ui: Componentes accesibles y personalizables",
+    url: "https://cursos.devtalles.com/certificates/mekejpsgkm",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Zustand: Gestor de estado para React",
+    url: "https://cursos.devtalles.com/certificates/kgagu4hzhb",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "TanStack Query - Un poderoso gestor de estado asíncrono.",
+    url: "https://cursos.devtalles.com/certificates/zzhjlqqw6r",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Astro: El framework para sitios web orientados al contenido",
+    url: "https://cursos.devtalles.com/certificates/o2m789nfgz",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "GIT+GitHub: Todo un sistema de control de versiones de cero",
+    url: "https://cursos.devtalles.com/certificates/x62pu4pedm",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "TypeScript: Tu completa guía y manual de mano.",
+    url: "https://cursos.devtalles.com/certificates/oc11zdv3hi",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "React: Aplicaciones en tiempo real con Socket-io",
+    url: "https://cursos.devtalles.com/certificates/hhbvykumda",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Principios SOLID y Clean Code",
+    url: "https://cursos.devtalles.com/certificates/seycnl25tc",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "JavaScript Moderno: Guía para dominar el lenguaje",
+    url: "https://cursos.devtalles.com/certificates/5ega81zny2",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Programación para principiantes - Primeros pasos",
+    url: "https://cursos.devtalles.com/certificates/vjzsgmzrlq",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "OpenAI: Ejercicios prácticos y asistentes con React + NestJS",
+    url: "https://cursos.devtalles.com/certificates/gxpbvtrmeo",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+  {
+    title: "Visual Studio Code: Mejora tu velocidad para codificar",
+    url: "https://cursos.devtalles.com/certificates/jgdygum41r",
+    logo: "https://import.cdn.thinkific.com/643563%2Fcustom_site_themes%2Fid%2FD0U6aCTrQPmlGzQXLZhe_DEVTALLES-LOGO-VARIANTES.png",
+  },
+].sort((a, b) => a.title.length - b.title.length);
