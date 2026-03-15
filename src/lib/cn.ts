7195fc0bb91b7cbd0ba0f5100a1dc3982d184b46
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-15 17:16:12 -0600

    docs: update documentation for cn utility function

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * @description: Utility function to merge class names using clsx and tailwind-merge.
+ * @param {...ClassValue[]} inputs - An array of class values to be merged.
+ * @returns {string} - A single string of merged class names.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
