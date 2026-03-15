48ddbaa9c9c574facd79b342f74d9b3ee30bb711
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-15 13:44:52 -0600

    refactor: remove layout references from blog post files and update data handling in components

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -1,8 +1,3 @@
 import type { InferEntrySchema } from "astro:content";
 
 export interface Post extends InferEntrySchema<"blog"> {}
-
-export interface PostGlob {
-  url: string;
-  frontmatter: Post;
-}
