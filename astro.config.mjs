cacebba6b4f27927ef6b60c5a6da279f524c1133
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-25 20:38:45 -0600

    feat: add MDX support and enhance blog layout
    - Added @astrojs/mdx dependency for MDX support.
    - Updated BlogPost component to adjust max-width for better responsiveness.
    - Modified AstroGreet component to include a new line for clarity.
    - Enhanced BlogLayout and Layout components to accept additional class properties for styling.
    - Improved PostLayout to include a sidebar for post headings.
    - Updated blog and tag pages to remove unnecessary max-width constraints.
    - Added smooth scrolling behavior in global CSS.
    - Created SideBarPost component to display headings for better navigation within posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 import preact from '@astrojs/preact';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
   trailingSlash: 'never',
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   adapter: cloudflare(),
   output: 'static',
   fonts: [
