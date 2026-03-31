df13da37d288e4d7c1193d58f5fdb8c3e1ad2d99
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-31 14:41:09 -0600

    feat: add initial live collection configuration with placeholder for loader and schema

---
+++ src/live.config.ts
@@ -0,0 +1,11 @@
+import { defineLiveCollection } from "astro:content";
+import { z } from "astro/zod";
+
+// const collectionName = defineLiveCollection({
+//   loader: CUSTOM_LOADER_FUNCTION (generate this in base of the documentation),
+//   schema: Z_TYPE_FOR_THE_LOADED_DATA (similar to content.config.ts schema),
+// });
+
+export const collections = {
+  // collectionName
+};
